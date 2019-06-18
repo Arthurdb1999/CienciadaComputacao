070acf76ab605ada8877f0b9ad7dd5ed3e993cba
Arthur de Bortoli <33229271+Arthurdb1999@users.noreply.github.com>
2019-06-17 21:04:12 -0300

    Add files via upload

---
+++ ProxyWizards/src/IvoryTower.java
@@ -0,0 +1,8 @@
+
+public class IvoryTower implements WizardTower {
+
+	@Override
+	public void enter(Wizard wizard) {
+		System.out.println(wizard.toString() + " enters the tower.");
+	}
+}
---
+++ ProxyWizards/src/WizardTower.java
@@ -0,0 +1,6 @@
+
+public interface WizardTower {
+	void enter(Wizard wizard);
+	
+
+}
