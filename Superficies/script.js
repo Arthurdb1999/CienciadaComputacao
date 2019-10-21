345416979f84b112771e76bf3f0f3ea3f2b8b424
arthurdb1999 <arthurdb1999@gmail.com>
2019-10-21 09:50:53 -0300

    Superficies de revolução e deslocamento

--- Superficies/script.js
+++ Superficies/script.js
@@ -33,21 +33,99 @@ function gerarQuadro(larg = 1, alt = 1){
     return geo;
 }
 
+function gerarCilindro(r = 1, alt = 1, pRaio = 4, pAlt = 1){
+    var geo = new THREE.Geometry();
+    var nc = 0, nl = 0
+    for(var y = -alt / 2; y <= alt/2; y += alt/pAlt){
+        nl++; nc = 0;
+        for(var a = 0; a <= Math.PI * 2; a += (Math.PI * 2) / pRaio) {
+            nc++;
+            var x = Math.sin(a) * r;
+            var z = Math.cos(a) * r;
+            var v = new THREE.Vector3(x, y, z);
+            geo.vertices.push(v);
+        }
+    }
+    for (var L = 0; L < nl - 1; L++) {
+        for (var c = 0; c < nc - 1; c += 1) {
+            geo.faces.push(new THREE.Face3( L*nc+c,
+                                            L*nc+c+1,
+                                            (L+1)*nc+c));
+            geo.faces.push(new THREE.Face3( (L+1)*nc+c+1,
+                                            (L+1)*nc+c,
+                                             L*nc+c+1));
+        }
+    }
+    geo.computeFaceNormals();
+    return geo
+}
+
+    function gerarSuperficiePlana(lar = 1, alt = 1, p = 1) {
+        var geo = new THREE.Geometry()
+        var nc = 0, nl = 0;
+        for (var y = -alt / 2; y <= alt / 2; y += alt / p) {
+            nl++;
+            nc = 0;
+            for (var x = -lar / 2; x <= lar / 2; x += lar / p){
+                nc++;
+                var v = new THREE.Vector3(x, y, 0);
+                geo.vertices.push(v);
+            }
+        }
+        for (var L = 0; L < nl - 1; L++){
+            for (var c = 0; c < nc - 1; c += 1) {
+                geo.faces.push(new THREE.Face3(
+                    L*nc + c, L*nc + c + 1, (L+1) * nc + c
+                ));
+                geo.faces.push(new THREE.Face3(
+                    (L+1)*nc + c + 1, (L+1)*nc + c, L*nc + c + 1
+                ));
+            }
+        }
+        geo.computeFaceNormals();
+        return geo;
+    }
+
+    var icosaedro = new THREE.IcosahedronGeometry();
+    var material = new THREE.MeshBasicMaterial({color: 0xffffff});
+    var icosaedrofinal = new THREE.Mesh(icosaedro, material);
+    icosaedrofinal.material.wireframe=true
+    //cena.add(icosaedrofinal);
+
+    var bola = new THREE.SphereGeometry(0.3, 16, 16);
+    var material = new THREE.MeshBasicMaterial({color: 0xff9942})
+    var sphere = new THREE.Mesh(bola, material);
+    sphere.material.wireframe=true;
+    //cena.add(sphere);
+
+    var geometry = new THREE.TorusKnotBufferGeometry(0.5, 0.5, 0.5, 0.5);
+    var material = new THREE.MeshBasicMaterial( { color: 0x009900 } );
+    var torusKnot = new THREE.Mesh( geometry, material );
+    torusKnot.position.x = -2
+    torusKnot.material.wireframe=true
+    //cena.add( torusKnot );
+
+    //Torus Knot não é uma superficie de revolução, é uma superfície de deslocamento
+
 var forma = new THREE.Mesh(
-    gerarQuadro(2, 2),
+    //gerarQuadro(2, 2),
+    //gerarCilindro(1, 2, 35, 4),
+    //new THREE.IcosahedronGeometry(),
+    gerarSuperficiePlana(2, 3, 5),
     new THREE.MeshPhongMaterial({ color: 0xffffff })
 );
 
-/*var forma = new LineSegments(
-    gerarCilindroLinhas(1, 2, 360),
-    gerarQuadro(2, 2),
-    new THREE.MeshPhongMaterial({ color: 0xffffff })
-);*/
+// var forma = new LineSegments(
+//     gerarCilindroLinhas(1, 2, 360),
+//     gerarQuadro(2, 2),
+//     new THREE.MeshPhongMaterial({ color: 0xffffff })
+// );
 
 //forma.material.wireframe = true;
 forma.material.side = THREE.DoubleSide;
 
 cena.add(forma);
+
 
 var luzAmbiente = new THREE.AmbientLight(0x333333);
 cena.add(luzAmbiente)
